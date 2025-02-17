c1aa325b83be012ab2232400d0041ea611ad4e33
SoucaThomas <thomassouca@gmail.com>
2025-02-17 22:16:24 +0200

    feat: update Navbar component to include a link to the homepage and adjust layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,18 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Car } from "lucide-react";
 import { NavUser } from "./NavUser";
+import Link from "next/link";
 
 export async function Navbar() {
   return (
-    <nav className="flex h-24 items-center justify-between border-b px-10 py-4">
-      <div className="flex flex-row items-center justify-center gap-2 text-primary">
+    <nav className="flex h-20 items-center justify-between border-b px-10 py-4">
+      <Link
+        href="/"
+        className="flex flex-row items-center justify-center gap-2 text-primary"
+      >
         <Car className="h-12 w-12" />
         <h1 className="text-4xl font-bold">CMP</h1>
-      </div>
+      </Link>
       <div className="flex flex-row items-center justify-center gap-6">
         <Input placeholder="Search" className="w-64 rounded-xl" />
         <Button className="rounded-xl bg-primary px-8 text-white">
