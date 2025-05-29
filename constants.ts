6a21f8f99a20bb963af0a4c2f35f0e8aadbb63cc
SoucaThomas <thomassouca@gmail.com>
2025-05-29 12:56:27 +0300

    refactor: streamline car listing form by enhancing mileage handling and removing redundant constants

---
+++ constants.ts
@@ -0,0 +1,544 @@
+export const countries = [
+  {
+    label: 'Andorra',
+    code: 'AD',
+    id: 1,
+  },
+  {
+    label: 'Albania',
+    code: 'AL',
+    id: 2,
+  },
+  {
+    label: 'Austria',
+    code: 'AT',
+    id: 3,
+  },
+  {
+    label: 'Åland Islands',
+    code: 'AX',
+    id: 4,
+  },
+  {
+    label: 'Bosnia and Herzegovina',
+    code: 'BA',
+    id: 5,
+  },
+  {
+    label: 'Belgium',
+    code: 'BE',
+    id: 6,
+  },
+  {
+    label: 'Bulgaria',
+    code: 'BG',
+    id: 7,
+  },
+  {
+    label: 'Belarus',
+    code: 'BY',
+    id: 8,
+  },
+  {
+    label: 'Switzerland',
+    code: 'CH',
+    id: 9,
+  },
+  {
+    label: 'Cyprus',
+    code: 'CY',
+    id: 10,
+  },
+  {
+    label: 'Czech Republic',
+    code: 'CZ',
+    id: 11,
+  },
+  {
+    label: 'Germany',
+    code: 'DE',
+    id: 12,
+  },
+  {
+    label: 'Denmark',
+    code: 'DK',
+    id: 13,
+  },
+  {
+    label: 'Estonia',
+    code: 'EE',
+    id: 14,
+  },
+  {
+    label: 'Spain',
+    code: 'ES',
+    id: 15,
+  },
+  {
+    label: 'Finland',
+    code: 'FI',
+    id: 16,
+  },
+  {
+    label: 'Faroe Islands',
+    code: 'FO',
+    id: 17,
+  },
+  {
+    label: 'France',
+    code: 'FR',
+    id: 18,
+  },
+  {
+    label: 'United Kingdom',
+    code: 'GB',
+    id: 19,
+  },
+  {
+    label: 'Guernsey',
+    code: 'GG',
+    id: 20,
+  },
+  {
+    label: 'Greece',
+    code: 'GR',
+    id: 21,
+  },
+  {
+    label: 'Croatia',
+    code: 'HR',
+    id: 22,
+  },
+  {
+    label: 'Hungary',
+    code: 'HU',
+    id: 23,
+  },
+  {
+    label: 'Ireland',
+    code: 'IE',
+    id: 24,
+  },
+  {
+    label: 'Isle of Man',
+    code: 'IM',
+    id: 25,
+  },
+  {
+    label: 'Iceland',
+    code: 'IC',
+    id: 26,
+  },
+  {
+    label: 'Italy',
+    code: 'IT',
+    id: 27,
+  },
+  {
+    label: 'Jersey',
+    code: 'JE',
+    id: 28,
+  },
+  {
+    label: 'Liechtenstein',
+    code: 'LI',
+    id: 29,
+  },
+  {
+    label: 'Lithuania',
+    code: 'LT',
+    id: 30,
+  },
+  {
+    label: 'Luxembourg',
+    code: 'LU',
+    id: 31,
+  },
+  {
+    label: 'Latvia',
+    code: 'LV',
+    id: 32,
+  },
+  {
+    label: 'Monaco',
+    code: 'MC',
+    id: 33,
+  },
+  {
+    label: 'Moldova, Republic of',
+    code: 'MD',
+    id: 34,
+  },
+  {
+    label: 'Macedonia, The Former Yugoslav Republic of',
+    code: 'MK',
+    id: 35,
+  },
+  {
+    label: 'Malta',
+    code: 'MT',
+    id: 36,
+  },
+  {
+    label: 'Netherlands',
+    code: 'NL',
+    id: 37,
+  },
+  {
+    label: 'Norway',
+    code: 'NO',
+    id: 38,
+  },
+  {
+    label: 'Poland',
+    code: 'PL',
+    id: 39,
+  },
+  {
+    label: 'Portugal',
+    code: 'PT',
+    id: 40,
+  },
+  {
+    label: 'Romania',
+    code: 'RO',
+    id: 41,
+  },
+  {
+    label: 'Russian Federation',
+    code: 'RU',
+    id: 42,
+  },
+  {
+    label: 'Sweden',
+    code: 'SE',
+    id: 43,
+  },
+  {
+    label: 'Slovenia',
+    code: 'SI',
+    id: 44,
+  },
+  {
+    label: 'Svalbard and Jan Mayen',
+    code: 'SJ',
+    id: 45,
+  },
+  {
+    label: 'Slovakia',
+    code: 'SK',
+    id: 46,
+  },
+  {
+    label: 'San Marino',
+    code: 'SM',
+    id: 47,
+  },
+  {
+    label: 'Ukraine',
+    code: 'UA',
+    id: 48,
+  },
+  {
+    label: 'Holy See (Vatican City State)',
+    code: 'VA',
+    id: 49,
+  },
+];
+
+export const fuelTypes = [
+  {
+    id: 1,
+    label: 'CNG',
+  },
+  {
+    id: 2,
+    label: 'Diesel',
+  },
+  {
+    id: 3,
+    label: 'Electricity',
+  },
+  {
+    id: 6,
+    label: 'Gasoline or E85',
+  },
+  {
+    id: 7,
+    label: 'Gasoline or natural gas',
+  },
+  {
+    id: 8,
+    label: 'Gasoline or propane',
+  },
+  {
+    id: 9,
+    label: 'Hydrogen',
+  },
+  {
+    id: 10,
+    label: 'Midgrade',
+  },
+  {
+    id: 11,
+    label: 'Premium',
+  },
+  {
+    id: 12,
+    label: 'Premium and Electricity',
+  },
+  {
+    id: 13,
+    label: 'Premium Gas or Electricity',
+  },
+  {
+    id: 14,
+    label: 'Premium or E85',
+  },
+  {
+    id: 15,
+    label: 'Regular',
+  },
+  {
+    id: 16,
+    label: 'Regular Gas and Electricity',
+  },
+  {
+    id: 17,
+    label: 'Regular Gas or Electricity',
+  },
+];
+
+export const colors = [
+  {
+    id: 1,
+    label: 'White',
+  },
+  {
+    id: 2,
+    label: 'Black',
+  },
+  {
+    id: 3,
+    label: 'Gray',
+  },
+  {
+    id: 4,
+    label: 'Silver',
+  },
+  {
+    id: 5,
+    label: 'Red',
+  },
+  {
+    id: 6,
+    label: 'Blue',
+  },
+  {
+    id: 7,
+    label: 'Brown',
+  },
+  {
+    id: 8,
+    label: 'Green',
+  },
+  {
+    id: 9,
+    label: 'Beige',
+  },
+  {
+    id: 10,
+    label: 'Yellow',
+  },
+  {
+    id: 11,
+    label: 'Gold',
+  },
+  {
+    id: 12,
+    label: 'Orange',
+  },
+  {
+    id: 13,
+    label: 'Purple',
+  },
+  {
+    id: 14,
+    label: 'Pink',
+  },
+  {
+    id: 15,
+    label: 'Maroon',
+  },
+  {
+    id: 16,
+    label: 'Navy',
+  },
+];
+
+export const driveType = [
+  {
+    id: 1,
+    label: 'Automatic (AM-S6)',
+  },
+  {
+    id: 2,
+    label: 'Automatic (variable gear ratios)',
+  },
+  {
+    id: 3,
+    label: 'Automatic (AM-S9)',
+  },
+  {
+    id: 4,
+    label: 'Manual 7-spd',
+  },
+  {
+    id: 6,
+    label: 'Automatic (AM5)',
+  },
+  {
+    id: 7,
+    label: 'Automatic (S8)',
+  },
+  {
+    id: 8,
+    label: 'Automatic (A2)',
+  },
+  {
+    id: 9,
+    label: 'Manual 4-spd',
+  },
+  {
+    id: 10,
+    label: 'Automatic (L4)',
+  },
+  {
+    id: 11,
+    label: 'Automatic 9-spd',
+  },
+  {
+    id: 12,
+    label: 'Automatic 7-spd',
+  },
+  {
+    id: 13,
+    label: 'Automatic 4-spd',
+  },
+  {
+    id: 14,
+    label: 'Automatic (AM8)',
+  },
+  {
+    id: 15,
+    label: 'Manual 3-spd',
+  },
+  {
+    id: 16,
+    label: 'Manual 4-spd Doubled',
+  },
+  {
+    id: 17,
+    label: 'Automatic (AM-S8)',
+  },
+  {
+    id: 18,
+    label: 'Automatic (AV-S8)',
+  },
+  {
+    id: 19,
+    label: 'Automatic (AM-S10)',
+  },
+  {
+    id: 20,
+    label: 'Automatic 10-spd',
+  },
+  {
+    id: 21,
+    label: 'Automatic (S6)',
+  },
+  {
+    id: 22,
+    label: 'Automatic (AM7)',
+  },
+  {
+    id: 23,
+    label: 'Automatic (L3)',
+  },
+  {
+    id: 24,
+    label: 'Automatic 6-spd',
+  },
+  {
+    id: 25,
+    label: 'Automatic (AV-S1)',
+  },
+  {
+    id: 26,
+    label: 'Manual 6-spd',
+  },
+  {
+    id: 27,
+    label: 'Automatic (AV-S6)',
+  },
+  {
+    id: 28,
+    label: 'Automatic (AV-S7)',
+  },
+  {
+    id: 29,
+    label: 'Automatic (AV-S10)',
+  },
+  {
+    id: 30,
+    label: 'Transmission',
+  },
+  {
+    id: 31,
+    label: 'Automatic (S10)',
+  },
+  {
+    id: 32,
+    label: 'Automatic 8-spd',
+  },
+  {
+    id: 33,
+    label: 'Manual 5-spd',
+  },
+  {
+    id: 34,
+    label: 'Automatic 5-spd',
+  },
+  {
+    id: 35,
+    label: 'Automatic (S4)',
+  },
+  {
+    id: 36,
+    label: 'Automatic (S7)',
+  },
+  {
+    id: 37,
+    label: 'Automatic 3-spd',
+  },
+  {
+    id: 38,
+    label: 'Automatic (S5)',
+  },
+  {
+    id: 39,
+    label: 'Automatic (AM-S7)',
+  },
+  {
+    id: 40,
+    label: 'Automatic (A1)',
+  },
+  {
+    id: 41,
+    label: 'Automatic (AM6)',
+  },
+  {
+    id: 42,
+    label: 'Automatic (S9)',
+  },
+];
